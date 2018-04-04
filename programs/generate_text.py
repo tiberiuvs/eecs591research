a43f10ce1f399198ff236e8fcf200d251c86c2e4
Tiberiu Vilcu <tiberiu.vilcu@icloud.com>
2018-04-04 17:07:20 -0400

    Split up generator into separate script to generate QR code or text hex dump

---
+++ programs/generate_text.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from Crypto.PublicKey import RSA
+from Crypto.Signature import PKCS1_PSS
+from Crypto.Hash import SHA
+import datetime
+import qrcode
+
+
+PRIVATE_KEY_LOC = 'sample_private_key.pem'
+TIMESTAMP_FORMAT = '%Y-%m-%dT%H:%M:%S.%f'
+DELIMITER = '0000'
+CODES_DIR = 'codes'
+
+# Message is timestamp and its hash
+timestamp = datetime.datetime.utcnow()
+message = timestamp.strftime(TIMESTAMP_FORMAT)
+mHash = SHA.new(message.encode('utf-8'))
+# Create an RSA signature with the private key
+privateKey = RSA.importKey(open(PRIVATE_KEY_LOC, 'r').read())
+signer = PKCS1_PSS.new(privateKey)
+signature = signer.sign(mHash)
+# Create a hex value message and print
+idString = message.encode('utf-8').hex() + DELIMITER + signature.hex()
+print(idString)
---
+++ programs/validate_text.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+from Crypto.PublicKey import RSA
+from Crypto.Signature import PKCS1_PSS
+from Crypto.Hash import SHA
+import datetime
+import sys
+
+
+PUBLIC_KEY_LOC = 'sample_public_key.pem'
+TIMESTAMP_FORMAT = '%Y-%m-%dT%H:%M:%S.%f'
+DELIMITER = '0000'
+TIMEOUT = 300
+
+
+assert len(sys.argv) > 1
+# Read in the given message and split up the message
+messageList = sys.argv[1].split(DELIMITER)
+timestamp = bytes.fromhex(messageList[0]).decode('utf-8')
+signature = bytes.fromhex(messageList[1])
+mHash = SHA.new(timestamp.encode('utf-8'))
+# Verify the RSA signature
+publicKey = RSA.importKey(open(PUBLIC_KEY_LOC, 'r').read())
+verifier = PKCS1_PSS.new(publicKey)
+if not verifier.verify(mHash, signature):
+    print('ININVALID')
+    sys.exit()
+# Verify the message is not stale
+messageTime = datetime.datetime.strptime(timestamp, TIMESTAMP_FORMAT)
+currentTime = datetime.datetime.utcnow()
+difference = (currentTime - messageTime).seconds
+if difference > TIMEOUT:
+    print('STALE')
+    sys.exit()
+print('VALID')
