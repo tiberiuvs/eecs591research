5f591aed34007911de9be67a0a02e81dffce5088
Hossein Golestani <hosseingolestani@yahoo.com>
2018-04-17 13:54:01 -0400

    More modifications

--- vm/create_chain.sh
+++ vm/create_chain.sh
@@ -5,9 +5,9 @@ DATA_DIR="$MULTICHAIN_DIR/server_data"
 MULTICHAIN_UTIL="$MULTICHAIN_DIR/multichain-util"
 MULTICHAIN_D="$MULTICHAIN_DIR/multichaind"
 
-./connect_internet.sh
+sudo ifdown enp0s8; sudo ifdown enp0s3; sudo ifup enp0s3
 sudo apt-get update > /dev/null
-./connect_internal.sh
+sudo ifdown enp0s3; sudo ifdown enp0s8; sudo ifup enp0s8
 mkdir $DATA_DIR
 $MULTICHAIN_UTIL create vote_chain -datadir=$DATA_DIR -default-network-port=8000 -anyone-can-connect=true -anyone-can-send=true -anyone-can-receive=true -root-stream-name=voting_stream -root-stream-open=true
 $MULTICHAIN_D vote_chain -datadir=$DATA_DIR -daemon
--- vm/run_chain.sh
+++ vm/run_chain.sh
@@ -7,5 +7,5 @@ MULTICHAIN_D="$MULTICHAIN_DIR/multichaind"
 sudo ifdown enp0s8; sudo ifdown enp0s3; sudo ifup enp0s3
 sudo apt-get update > /dev/null
 sudo ifdown enp0s3; sudo ifdown enp0s8; sudo ifup enp0s8
-mkdir $DATA_DIR
-$MULTCHAIN_D vote_chain@192.168.15.11:8000 -datadir=$DATA_DIR -daemon
+
+$MULTCHAIN_D vote_chain -datadir=$DATA_DIR -daemon
