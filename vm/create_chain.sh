153eb2b854930a1a4faa341329bdc6791e6484f6
Hossein Golestani <hosseingolestani@yahoo.com>
2018-04-17 12:39:35 -0400

    Fix minor bugs

--- vm/create_chain.sh
+++ vm/create_chain.sh
@@ -5,6 +5,6 @@ MULTICHAIN_DIR="/home/eecs591/multichain-1.0.4/"
 ./connect_internet.sh
 sudo apt-get update > /dev/null
 ./connect_internal.sh
-mkdir $MULTCHAINDIR"server_data"
-$MULTCHAINDIR"multichain-util" create vote_chain -datadir=$MULTCHAINDIR"server_data" -default-network-port=8000 -anyone-can-connect=true -anyone-can-send=true -anyone-can-receive=true -root-stream-name=voting_stream -root-stream-open=true
-$MULTCHAINDIR"multichaind" vote_chain -datadir=$MULTCHAINDIR"server_data" -daemon
+mkdir $MULTCHAIN_DIR"server_data"
+$MULTCHAIN_DIR"multichain-util" create vote_chain -datadir=$MULTCHAIN_DIR"server_data" -default-network-port=8000 -anyone-can-connect=true -anyone-can-send=true -anyone-can-receive=true -root-stream-name=voting_stream -root-stream-open=true
+$MULTCHAIN_DIR"multichaind" vote_chain -datadir=$MULTCHAIN_DIR"server_data" -daemon
--- vm/join_chain.sh
+++ vm/join_chain.sh
@@ -5,5 +5,5 @@ MULTICHAIN_DIR="/home/eecs591/multichain-1.0.4/"
 ./connect_internet.sh
 sudo apt-get update > /dev/null
 ./connect_internal.sh
-mkdir $MULTCHAINDIR"server_data"
-$MULTCHAINDIR"multichaind" vote_chain@192.168.15.11:8000 -datadir=$MULTCHAINDIR"server_data" -daemon
+mkdir $MULTCHAIN_DIR"server_data"
+$MULTCHAIN_DIR"multichaind" vote_chain@192.168.15.11:8000 -datadir=$MULTCHAIN_DIR"server_data" -daemon
