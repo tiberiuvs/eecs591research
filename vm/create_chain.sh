774d2811e76fbc42b0a954c5bad87c5393781bc5
Hossein Golestani <hosseingolestani@yahoo.com>
2018-04-18 00:13:19 -0400

    Trying new blockchain parameters

--- vm/create_chain.sh
+++ vm/create_chain.sh
@@ -9,5 +9,5 @@ sudo ifdown enp0s8; sudo ifdown enp0s3; sudo ifup enp0s3
 sudo apt-get update > /dev/null
 sudo ifdown enp0s3; sudo ifdown enp0s8; sudo ifup enp0s8
 mkdir $DATA_DIR
-$MULTICHAIN_UTIL create vote_chain -datadir=$DATA_DIR -default-network-port=8000 -anyone-can-connect=true -anyone-can-send=true -anyone-can-receive=true -root-stream-name=voting_stream -root-stream-open=true
+$MULTICHAIN_UTIL create vote_chain -datadir=$DATA_DIR -default-network-port=8000 -anyone-can-connect=true -anyone-can-send=true -anyone-can-receive=true -root-stream-name=voting_stream -root-stream-open=true -anyone-can-issue=true -anyone-can-mine=true -anyone-can-activate=true -anyone-can-admin=true
 $MULTICHAIN_D vote_chain -datadir=$DATA_DIR -daemon
