37eca6714848e00ef9c167ffa5d81406922d7274
Hossein Golestani <hosseingolestani@yahoo.com>
2018-04-20 04:19:15 -0400

    Adding script for putting a transaction to the stream

---
+++ vm/dummy_key.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+MULTICHAIN_DIR="/home/eecs591/multichain-1.0.4"
+DATA_DIR="$MULTICHAIN_DIR/server_data"
+MULTICHAIN_CLI="$MULTICHAIN_DIR/multichain-cli"
+
+$MULTICHAIN_CLI vote_chain -datadir=$DATA_DIR publish voting_stream "dummy_key_"$1 123456
+$MULTICHAIN_CLI vote_chain -datadir=$DATA_DIR liststreamkeys voting_stream
